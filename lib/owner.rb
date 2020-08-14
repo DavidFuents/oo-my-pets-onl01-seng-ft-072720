cde44f286d51ae6073d7509ed8570003ac8af6a7
David Fuentes <davidf0827@gmail.com>
2020-08-14 22:46:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,11 +30,11 @@ class Owner
   end
   
   def walk_dogs
-    Dog.all.each {|dog| Dog.mood = "happy"}
+    Dog.all.each {|dog| dog.mood = "happy"}
   end
   
   def feed_cats 
-    Cat.all.each {|cat| Cat.mood = "happy"}
+    Cat.all.each {|cat| cat.mood = "happy"}
   end
   
   def sell_pets
